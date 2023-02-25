20c316c5b91dcf1b0d4b8999cd906cc6ca2b19a7
Jean Benecyo <japolea@gmail.com>
2023-02-24 18:13:21 -0600

    first commit

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
 // ini_set('display_startup_errors', '1');
 // error_reporting(E_ALL);
 
-require './functions.php';
+require './inc/functions.php';
 
 $id = isset($_GET['id']) ? secure_input($_GET['id']) : null;
 $name = isset($_GET['n']) ? secure_input($_GET['n']) : null;
